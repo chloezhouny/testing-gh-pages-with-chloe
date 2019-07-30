bc538aa09e062fe601384ce123c8af512298096f
Esar Behlum <esarnb@gmail.com>
2019-07-30 16:58:02 -0700

    test 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {findAll, sendQuery}from '../src/Utils';
+import findAll from '../src/Utils';
 
 class App extends React.Component {
 
